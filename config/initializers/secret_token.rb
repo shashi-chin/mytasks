2f42871ec7ecabc4a6fcc4cfc195ff8366f6175e
shashi <shashi.chin@gmail.com>
2013-02-12 16:47:47 +1000

    adding mytasks to git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mytasks::Application.config.secret_token = '79cc77f05898c2f3c92939d9bd45f9d96b567b11c5ae82c96336f22130f021ad0657952544648d6dd536e9e439565f7c38135aa42010660464a083212df0b609'
